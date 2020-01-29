50ac5b82175b39e737b74cd0c56fbfac466e5d49
Quimey Marquez <qmarquez@est.frba.utn.edu.ar>
2020-01-29 15:09:30 -0300

    Class 30 - Promises practice.

--- README.md
+++ README.md
@@ -5,13 +5,24 @@ Just console simple app.
 ### -> Callback
 > A function who serves as argument to another function, the second one can call it.
 
-| Advantages                        | Disadvantages                                           |
-| --------------------------------- | ------------------------------------------------------- |
+| Advantages | Disadvantages |
+| ---------- | ------------- |
 | **Simple**, conceptually          | **Toscas**, when nesting many (Callback hell)           |
 | **Universal**, they are JS native | **Little intuitive**, to understand the flow is tedious |
 
 #### Notes
 *· First callback argument allways will be the error.*
+
+### -> Promise
+> **_Object_** wich represent the end or fail of a asinconous operation.
+
+| Advantages | Disadvantages |
+| ---------- | ------------- |
+| **Easily chaining**, clair and easy to chain hadling a flow | **No exceptions**, require .catch() method to handel errors |
+| **Powerfull**, really capable to handle complex async ops   | |
+
+#### Notes
+*· use promise instead callbacks. Watch out with promise hell.*
 
 ---
 #NodeJS\
--- index.js
+++ index.js
@@ -16,42 +16,35 @@ const mock_authors = [
   { id: 2, name: 'Steve Forest' }
 ]
 
-function getBookById(id, callback) {
-  const book = mock_books.find(book => book.id === id);
-  if (!book) {
-    const error = new Error('Book not found!');
-    return callback(error);
-  }
-
-  callback(null, book);
-}
-
-function getAuthorById(id, callback) {
-  const author = mock_authors.find(author => author.id === id);
-  if (!author) {
-    const error = new Error('Author not found!');
-    return callback(error);
-  }
-
-  callback(null, author);
-}
-
-getBookById(2, (error, book) => {
-  if (error) {
-    return console.log(error.message);
-  }
-
-  // Starting callbakc hell
-  getAuthorById(book.author_id, (error, author) => {
-    if (error) {
-      return console.log(error.message);
+function getBookById(id) {
+  return new Promise((resolve, reject) => {
+    const book = mock_books.find(book => book.id === id);
+    if (!book) {
+      const error = new Error('Book not found!');
+      reject(error);
     }
 
-    //  If i need to looking for another thing, a function inside function inside so on.
-    // So callback hell.
-
-    return console.log(`This book '${book.title}' was writen by ${author.name}`)
+    resolve(book);
   });
+}
 
-  return console.log(book);
-});
+function getAuthorById(id) {
+  return new Promise((resolve, reject) => {
+    const author = mock_authors.find(author => author.id === id);
+    if (!author) {
+      const error = new Error('Author not found!');
+      reject(error);
+    }
+
+    resolve(author);
+  })
+}
+
+getBookById(11)
+  .then(book => {
+    return getAuthorById(book.id);
+  })
+  .then(author => {
+    return console.log(author);
+  })
+  .catch(console.log);
