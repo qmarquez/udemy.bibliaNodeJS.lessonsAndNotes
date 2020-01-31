f6caebcd4e88fc8bc909959c691694c03444669c
Quimey Marquez <qmarquez@est.frba.utn.edu.ar>
2020-01-31 11:35:41 -0300

    Class 47 - Express.js

--- index.js
+++ index.js
@@ -1,24 +1,6 @@
-const http = require('http');
-const fs = require('fs');
 
-const home = fs.readFileSync('./index.html');
-const about = fs.readFileSync('./about.html');
+const server = require('express')();
 
-http
-  .createServer((req, res) => {
-    const { url } = req;
-
-    if (url === '/') {
-      res.writeHead(200, { 'Content-Type': 'text/html' });
-      res.write(home);
-    } else if (url === '/about') {
-      res.writeHead(200, { 'Content-Type': 'text/html' });
-      res.write(about);
-    } else {
-      res.writeHead(404, { 'Content-Type': 'text/html' });
-      res.write('page not found');
-    }
-
-    res.end();
-  })
-  .listen(8081);
+server.listen(8081, () => {
+  console.log('Server running on port 8081');
+});
