878daf09e6d4c04f84af601c6e0b8e38cfd97f70
Quimey Marquez <qmarquez@est.frba.utn.edu.ar>
2020-02-06 17:59:15 -0300

    Class 75 - Navbar config.

--- S10 - Frontend and backend/project - coding app/frontend/src/app/app-routing.module.ts
+++ S10 - Frontend and backend/project - coding app/frontend/src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { TechnologiesComponent } from './pages/technologies/technologies.component';
+import { TechnologyComponent } from './pages/technology/technology.component';
+import { SearchComponent } from './pages/search/search.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'technologies', component: TechnologiesComponent },
+  { path: 'technology/:id', component: TechnologyComponent },
+  { path: 'seacrh/:query', component: SearchComponent },
+  { path: '**', pathMatch: 'full', redirectTo: 'home' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
